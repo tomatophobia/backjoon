a61dc342e45de5e4b416c70c55410bdb1f2778d4
Youngseo Choi <ys550499@gmail.com>
2021-12-01 23:56:42 +0900

    10828 스택

---
+++ 10828.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+st = []
+for i in range(t):
+    cmd = input().rstrip().split(' ')
+    if cmd[0] == 'push':
+       st.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if len(st) == 0:
+            print(-1)
+        else:
+            print(st[-1])
+            st.pop()
+    elif cmd[0] == 'size':
+        print(len(st))
+    elif cmd[0] == 'empty':
+        if len(st) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'top':
+        if len(st) == 0:
+            print(-1)
+        else:
+            print(st[-1])
